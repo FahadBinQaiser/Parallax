0a402315e0a371589cb2ff6d40c0bd4e24275843
Fahad <fahadbinqaiser3@gmail.com>
2025-09-16 21:43:48 +0500

    Making changes

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 export default function HeroSection() {
   return (
-    <div className="heroSection flex flex-col text-[#000000]/70 gap-5 justify-center items-center h-screen">
+    <div className="heroSection flex flex-col text-[#000000]/70 gap-5 justify-center items-center h-screen pt-16">
       <p className="text-2xl text-center max-w-xl font-medium">
         Trusted by growth-focused SaaS teams worldwide
       </p>
-      <h1 className="text-8xl text-center  font-bold">
+      <h1 className="text-8xl text-center font-bold">
         Your{" "}
         <span className="bg-[#FB2B8F] inline-block px-2 text-white">
           AI teammate
@@ -17,6 +17,9 @@ export default function HeroSection() {
         Automate repetitive tasks with an AI Agent built for SaaS teams <br />{" "}
         so your people focus on strategy, not busywork.
       </p>
+      <button className="cursor-pointer rounded-3xl bg-[#FB2B8F] w-44 h-16 text-white text-2xl">
+        Book a Call
+      </button>
     </div>
   );
 }
