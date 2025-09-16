09449ee8d069070a63d9c8037257dc198276f9d0
Fahad <fahadbinqaiser3@gmail.com>
2025-09-16 21:26:26 +0500

    Herosection setup

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,8 +1,22 @@
 import React from "react";
 export default function HeroSection() {
   return (
-    <div className="heroSection text-white">
-      <h1>Parallax Scrolling Effect</h1>
+    <div className="heroSection flex flex-col text-[#000000]/70 gap-5 justify-center items-center h-screen">
+      <p className="text-2xl text-center max-w-xl font-medium">
+        Trusted by growth-focused SaaS teams worldwide
+      </p>
+      <h1 className="text-8xl text-center  font-bold">
+        Your{" "}
+        <span className="bg-[#FB2B8F] inline-block px-2 text-white">
+          AI teammate
+        </span>
+        <br />
+        that never sleeps.{" "}
+      </h1>
+      <p className="text-2xl text-center max-w-xl">
+        Automate repetitive tasks with an AI Agent built for SaaS teams <br />{" "}
+        so your people focus on strategy, not busywork.
+      </p>
     </div>
   );
 }
