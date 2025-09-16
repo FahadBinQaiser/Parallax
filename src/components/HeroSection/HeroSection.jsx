0696b2e933319b74ae4cfd93bc973440eb9864e5
Fahad <fahadbinqaiser3@gmail.com>
2025-09-16 20:35:35 +0500

    Herosection started

---
+++ src/components/HeroSection/HeroSection.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+export default function HeroSection() {
+  return (
+    <div className="heroSection text-white">
+      <h1>Parallax Scrolling Effect</h1>
+    </div>
+  );
+}
