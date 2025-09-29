390e66f44f372e024c9390e95f120597911be8c7
Fahad <fahadbinqaiser3@gmail.com>
2025-09-29 19:51:43 +0500

    Adjusting styles

--- src/components/FeatureSection/FeatureSection.jsx
+++ src/components/FeatureSection/FeatureSection.jsx
@@ -18,7 +18,7 @@ export default function FeaturesSection() {
     },
     {
       icon: <Clock className="text-[#FB6F92] w-12 h-12" />,
-      title: "Always-on reliability",
+      title: "Always on reliability",
       desc: "It doesn’t sleep, forget, or burn out. Tasks are executed consistently, day and night, giving you complete confidence that nothing slips through the cracks.",
       size: "col-span-1 row-span-1",
     },
@@ -31,9 +31,9 @@ export default function FeaturesSection() {
   ];
 
   return (
-    <section className="text-[#111111] py-20 px-6 md:px-0">
+    <section className="text-[#000000]/70 py-20 px-6 md:px-0">
       <div className="max-w-6xl mx-auto space-y-12">
-        <h2 className="text-5xl sm:text-6xl md:text-7xl font-bold text-gray-900 leading-snug">
+        <h2 className="text-5xl sm:text-6xl md:text-7xl font-bold leading-tight">
           What our <span className="text-[#FB6F92]">AI agent</span> handles
           <br /> so you don’t have to:
         </h2>
@@ -43,15 +43,11 @@ export default function FeaturesSection() {
             <div key={idx} className={`${size}`}>
               <Gradient
                 index={idx}
-                className="flex flex-col justify-center items-start p-8 bg-white rounded-2xl shadow-lg border border-gray-100 hover:border-[#FB6F92] hover:shadow-pink-100 transition transform hover:-translate-y-2 h-full w-full"
+                className="flex flex-col justify-center items-start p-8 bg-white rounded-2xl shadow-lg border border-gray-100 hover:border-[#FB6F92] hover:shadow-pink-100 transition transform hover:-translate-y-6 h-full w-full"
               >
                 <div className="mb-4">{icon}</div>
-                <h3 className="text-4xl font-semibold text-gray-900 leading-snug">
-                  {title}
-                </h3>
-                <p className="mt-3 text-base md:text-lg text-gray-700">
-                  {desc}
-                </p>
+                <h3 className="text-4xl font-semibold leading-snug">{title}</h3>
+                <p className="mt-3 text-base md:text-lg">{desc}</p>
               </Gradient>
             </div>
           ))}
