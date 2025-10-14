15146c208905885c888d7e3085d083ad4ebcdca3
FahadBinQaiser <fahadbinqaiser3@gmail.com>
2025-10-15 00:47:24 +0500

    Changing information

--- src/components/PricingSection/PricingSection.jsx
+++ src/components/PricingSection/PricingSection.jsx
@@ -3,11 +3,12 @@ import PricingCard from "../UI_Components/PricingCard";
 
 export default function PricingSection() {
   return (
-    <section className="py-20 pricing-section">
+    <section className="py-20 text-[#000000]/70 pricing-section">
       <div className="max-w-4xl space-y-10 mx-auto px-6 text-center">
-        <h2 className="text-3xl font-bold sm:text-7xl">
-          Public <span className="highlight text-[#FB2B8F]">Cheers</span> for
-          Us!
+        <h2 className="text-5xl font-bold sm:text-7xl">
+          Serious{" "}
+          <span className="highlight text-[#FB2B8F]">About Growth?</span> So Are
+          We
         </h2>
         <div className="pricingCards gap-2 flex flex-row">
           <PricingCard
