2034582d15655922e48d18142e9573cfbb5d6c96
FahadBinQaiser <fahadbinqaiser3@gmail.com>
2025-10-14 11:02:23 +0500

    Adjusting stuff with card

--- src/components/PricingSection/PricingSection.jsx
+++ src/components/PricingSection/PricingSection.jsx
@@ -3,10 +3,13 @@ import PricingCard from "../UI_Components/PricingCard";
 
 export default function PricingSection() {
   return (
-    <section>
-      <h2>Pricing Plans</h2>
-      <div>
-        <PricingCard />
+    <section className="py-20 pricing-section">
+      <div className="max-w-4xl space-y-10 mx-auto px-6 text-center">
+        <h2>Pricing Plans</h2>
+        <div className="pricingCards gap-2 flex flex-row">
+          <PricingCard />
+          <PricingCard />
+        </div>
       </div>
     </section>
   );
