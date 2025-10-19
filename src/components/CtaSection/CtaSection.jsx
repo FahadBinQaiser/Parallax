5b08a777af859c5ce9710f3449ee409b3a42bb00
FahadBinQaiser <fahadbinqaiser3@gmail.com>
2025-10-19 18:39:13 +0500

    Imported the original button

--- src/components/CtaSection/CtaSection.jsx
+++ src/components/CtaSection/CtaSection.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-
+import StyledWrapper from "../UI_Components/StyledWrapper";
 export default function CtaSection() {
   return (
     <section className="relative h-full flex flex-col md:flex-row items-center justify-center text-center py-16 bg-gradient-to-b from-[#080008] via-[#0a0a0a] to-[#100010]">
       <div className="absolute inset-0 bg-[radial-gradient(ellipse_at_top_right,rgba(251,43,143,0.35)_0%,transparent_80%),radial-gradient(ellipse_at_bottom_left,rgba(255,110,196,0.2)_0%,transparent_90%)] pointer-events-none"></div>
 
       <div className="relative z-10 px-8 sm:px-16 md:px-16 py-16 md:py-0 text-white space-y-3">
-        <h2 className="text-8xl font-bold leading-tight">
+        <h2 className="text-6xl text-balance md:text-8xl font-bold leading-tight">
           Ready to{" "}
           <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#fb2b8f] via-[#ff6ec4] to-[#e14f9c]">
             Scale Smarter
@@ -21,9 +21,7 @@ export default function CtaSection() {
         </p>
 
         <div className="pt-6">
-          <button className="w-56 sm:w-64 py-4 text-lg rounded-full text-white bg-gradient-to-r from-[#fb2b8f] via-[#ff6ec4] to-[#e14f9c] shadow-[0_0_24px_rgba(251,43,143,0.5)] transition-all cursor-pointer duration-300 hover:scale-105 hover:shadow-[0_0_32px_rgba(251,43,143,0.7)]">
-            Book a Call
-          </button>
+          <StyledWrapper>Book a Call</StyledWrapper>
         </div>
       </div>
     </section>
