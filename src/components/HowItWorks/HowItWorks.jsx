c152418688762cc3449a96013afd5dccd4326a9e
FahadBinQaiser <fahadbinqaiser3@gmail.com>
2025-12-19 21:06:48 +0500

    Removed leading tight

--- src/components/HowItWorks/HowItWorks.jsx
+++ src/components/HowItWorks/HowItWorks.jsx
@@ -27,7 +27,7 @@ export default function HowItWorks() {
     <section className="py-16 md:py-24 px-6 relative">
       <div className="max-w-6xl mx-auto text-[#000000]/70">
         <div className="text-center mb-16 md:mb-24">
-          <h2 className="text-4xl md:text-6xl lg:text-7xl font-bold mb-4 md:mb-6 leading-20">
+          <h2 className="text-4xl md:text-6xl lg:text-7xl font-bold mb-4 md:mb-6">
             Setup in <span className="text-[#FB6F92]">minutes</span>,<br />
             not months
           </h2>
