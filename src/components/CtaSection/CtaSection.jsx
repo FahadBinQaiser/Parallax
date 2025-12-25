3181f31ee4a1c47990b96d493da517643d57128f
FahadBinQaiser <fahadbinqaiser3@gmail.com>
2025-12-25 11:47:25 +0500

    Improving Cta section

--- src/components/CtaSection/CtaSection.jsx
+++ src/components/CtaSection/CtaSection.jsx
@@ -6,22 +6,24 @@ export default function CtaSection() {
       <div className="absolute inset-0 bg-[radial-gradient(ellipse_at_top_right,rgba(251,43,143,0.35)_0%,transparent_80%),radial-gradient(ellipse_at_bottom_left,rgba(255,110,196,0.2)_0%,transparent_90%)] pointer-events-none"></div>
 
       <div className="relative z-10 px-8 sm:px-16 md:px-16 py-16 md:py-0 text-white space-y-3">
-        <h2 className="text-6xl text-balance md:text-8xl font-bold leading-tight">
-          Ready to{" "}
-          <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#fb2b8f] via-[#ff6ec4] to-[#e14f9c]">
-            Scale Smarter
-          </span>
-          ?
+        <h2 className="text-6xl text-balance md:text-6xl max-w-4xl mx-auto font-bold">
+          Stop wasting <span className="text-[#fb2b8f]">time on busywork</span>{" "}
+          Let AI handle it for you.
         </h2>
 
-        <p className="text-white/70 text-base sm:text-lg max-w-lg mx-auto leading-relaxed">
-          Automate the busywork, remove bottlenecks, and let your team focus on
-          the insights that drive growth. Fast setup, expert support, measurable
-          ROI.
+        <p className="text-gray-300 text-balance text-base sm:text-lg max-w-4xl mx-auto">
+          Your team can't waste hours on busywork. Let AI handle the repetitive
+          work so they can focus on the strategic work that actually grows your
+          business.
         </p>
 
-        <div className="pt-6">
-          <StyledWrapper>Book a Call</StyledWrapper>
+        <div className="pt-6 flex flex-row justify-center items-center">
+          <button className="p-3 py-3.5 bg-black/20 text-white rounded-xl">
+            See what gets automated
+          </button>
+          <StyledWrapper width={"full"} text="Book a Call">
+            Book a Call
+          </StyledWrapper>
         </div>
       </div>
     </section>
