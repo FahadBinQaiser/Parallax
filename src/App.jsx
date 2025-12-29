cf79039111e4a10298c68c2384f9f774858ca0f1
FahadBinQaiser <fahadbinqaiser3@gmail.com>
2025-12-29 12:00:07 +0500

    Vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Analytics } from "@vercel/analytics/react";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import HeroSection from "./components/HeroSection/HeroSection";
